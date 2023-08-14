6fc28d636a0a11f6607be73c6211dcdcec560bbf
Сергей Дубовский <svdub@mail.ru>
2023-08-14 11:24:44 +0300

    di-annotation

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -19,7 +19,7 @@ public class MainServlet extends HttpServlet {
 
   @Override
   public void init() {
-    final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+    final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("ru.netology");
     controller = context.getBean(PostController.class);
   }
 
